128c0a5368f10d87dd43a00130b259df8688d6ff
RaphaBane <raphaelaragaoparreira@gmail.com>
2025-07-06 17:00:08 -0300

    wip: deploy no Render

--- config/settings.py
+++ config/settings.py
@@ -27,12 +27,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-movu%p!#@@6p4&zqe3le+@!-r8yx@j1xkt55a3=8=#smare*oh'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(',')
 
 
 # Application definition
